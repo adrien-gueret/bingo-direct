f47f5a829b9cbe3085c5d9b6a0daacad94a4c139
Adrien Guéret <adrien.grt@gmail.com>
2026-09-05 16:59:08 +0200

    chore(*): copy configuration from mkwg

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import mkcert from "vite-plugin-mkcert";
+import { resolve } from "path";
 
 export default defineConfig({
-  plugins: [react()],
-  base: './',
-})
+  base: "/bingo-direct/",
+  plugins: [react(), mkcert()],
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "./src"),
+    },
+  },
+  publicDir: "public",
+  build: {
+    outDir: "dist",
+    assetsDir: "./",
+  },
+});
